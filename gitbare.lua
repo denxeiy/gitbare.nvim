07f69720c7538a59f0e9e165b453a09652e1ed6b
denxeiy <bashknf@gmail.com>
2026-05-13 20:17:50 +0300

    Little chore 2

--- gitbare.lua
+++ gitbare.lua
@@ -1,27 +1,26 @@
 return {
     "denxeiy/gitbare.nvim",
     dependencies = {
-        "telescope-nvim/telescope.nvim",
-        "telescope-nvim/telescope-file-browser.nvim",
-        "nvim-tree/nvim-web-devicons",
+        "telescope-nvim/telescope.nvim", -- dependency for beautiful UI windows and navigation
+        "nvim-tree/nvim-web-devicons", -- dependency for beautoful colorful icons
     },
     config = function ()
-        require("gitbare").setup({
+        require("gitbare").setup({ -- setup is strongly required
             dir = "/home/user", -- or any other directory. i prefer to use $HOME
-            git_dir_name = ".git", -- or .any, depends on how you named it
+            git_dir_name = ".git", -- or .*any*, depends on how you named it
             find_in_current_dir = false, -- false - search system-wide in your bare git repo, true - only in current dir
             grep_in_current_dir = false, -- same as above but for live grep
         })
         vim.keymap.set("n", "<leader>cb", function() -- file browser, you can put keymap in your separate file with mappings or change keys here
-                require("gitbare").gitbare_browser()
-        end, { desc = "GitBare File Browser" })
+                require("gitbare").gitbare_browser() -- if you will set keymap somewhere else - do it with this function()
+        end, { desc = "GitBare File Browser" }) -- if you don't need this bind - just comment it with "--" on left side
 
-        vim.keymap.set("n", "<leader>cf", function()
-            require("telescope.builtin").gitbare_find_files() -- find files, you can put keymap in your separate file with mappings or change keys here
-        end, { desc = "GitBare Find Files" })
+        vim.keymap.set("n", "<leader>cf", function() -- find files, you can put keymap in your separate file with mappings or change keys here
+            require("telescope.builtin").gitbare_find_files() -- if you will set keymap somewhere else - do it with this function()
+        end, { desc = "GitBare Find Files" }) -- if you don't need this bind - just comment it with "--" on left side
 
-        vim.keymap.set("n", "<leader>cg", function()
-            require("telescope.builtin").gitbare_live_grep() -- live grep, you can put keymap in your separate file with mappings or change keys here
-        end, { desc = "GitBare Live Grep" })
-    end,
+        vim.keymap.set("n", "<leader>cg", function() -- live grep, you can put keymap in your separate file with mappings or change keys here
+            require("telescope.builtin").gitbare_live_grep() -- if you will set keymap somewhere else - do it with this function()
+        end, { desc = "GitBare Live Grep" }) -- if you don't need this bind - just comment it with "--" on left side
+    end, -- at the end of installation you can remove all of my silly comments like this one :)
 }
